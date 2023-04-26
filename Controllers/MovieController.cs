ca569ef12ffd9200b911d5a6c1a03dc62908855a
Leirannt <93717185+Leirannt@users.noreply.github.com>
2023-04-26 17:03:53 +0700

    membuat semua elemen

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace modul9_1302210128.Controllers
+{
+    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private static List<Movie> film = new List<Movie>()
+        {
+            new Movie("The Shawshank Redemption",  "Frank Darabont",  new List<string>(){"Tim Robbins", "Morgan Freeman", "Bob Gunton"}, "Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit."),
+            new Movie("The Godfather",  "Frank Darabont",  new List<string>(){"Marlon Brando", "Al Pacino", "James Caan"},  "The Godfather 'Don' Vito Corleone is the head of the Corleone mafia family in New York. He is at the event of his daughter's wedding. Michael, Vito's youngest son and a decorated WW II Marine is also present at the wedding"),
+            new Movie( "The Dark Knight",  "Christopher Nolan",  new List<string>(){"Christian Bale", "Heath Ledger", "Aaron Eckhart"},  "Set within a year after the events of Batman Begins (2005), Batman, Lieutenant James Gordon, and new District Attorney Harvey Dent successfully begin to round up the criminals that plague Gotham City, until a mysterious and sadistic criminal mastermind known only as 'The Joker' appears in Gotham, creating a new wave of chaos.") 
+        };
+
+        [HttpGet]
+        public IEnumerable<Movie> GET()
+        {
+            return film;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Movie ada)
+        {
+            film.Add(ada);
+        }
+
+        [HttpGet("{id}")]
+        public Movie Get(int id)
+        {
+            return film[id];
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            film.RemoveAt(id);
+        }
+    }
+
+
+}
---
+++ Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace modul9_1302210128
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public List<string> Stars { get; set; }
+        public string description { get; set; }
+
+        public Movie(string Title, string Director, List<string> Stars, string description)
+        {
+            this.Title = Title;
+            this.Director = Director;
+            this.Stars = Stars;
+            this.description = description;
+        }
+    }
+
+}
